3c1606254a900f7d7652d97bed7d68f413575516
Josef Friedrich <josef@friedrich.rocks>
2015-08-07 18:19:54 +0200

    Add examples folder.

--- test-files/get.sh
+++ test-files/get.sh
@@ -1,3 +1,7 @@
 #! /bin/sh
 
+# dtx
 wget https://raw.githubusercontent.com/Josef-Friedrich/cloze/master/cloze.dtx
+
+# sty
+wget https://raw.githubusercontent.com/gpoore/minted/master/source/minted.sty
