1b00692d9902df7ab0cce7ce6840ab2cbba3d363
Josef Friedrich <josef@friedrich.rocks>
2015-08-08 12:35:12 +0200

    Add test files.

--- test-files/get.sh
+++ test-files/get.sh
@@ -2,6 +2,17 @@
 
 # dtx
 wget https://raw.githubusercontent.com/Josef-Friedrich/cloze/master/cloze.dtx
+wget https://raw.githubusercontent.com/huxuan/resumecls/master/resumecls.dtx
+wget http://mirrors.ctan.org/macros/latex/contrib/soul/soul.dtx
 
 # sty
 wget https://raw.githubusercontent.com/gpoore/minted/master/source/minted.sty
+wget http://mirrors.ctan.org/macros/latex/contrib/outline/outline.sty
+
+# cls
+wget http://mirrors.ctan.org/macros/latex/contrib/hitec/hitec.cls
+wget http://mirrors.ctan.org/macros/latex/contrib/hletter/hletter.cls
+
+# LaTeX
+
+wget http://mirrors.ctan.org/macros/latex/contrib/outline/outline.tex
