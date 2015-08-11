7a16b299dcae57475a450430e39157a9dd303e19
Josef Friedrich <josef@friedrich.rocks>
2015-08-11 13:30:36 +0200

    Langue files for sty and cls.

--- tests/integration/get.sh
+++ tests/integration/get.sh
@@ -8,11 +8,12 @@ wget -NP dtx http://mirrors.ctan.org/macros/latex/contrib/soul/soul.dtx
 # sty
 wget -NP sty https://raw.githubusercontent.com/gpoore/minted/master/source/minted.sty
 wget -NP sty http://mirrors.ctan.org/macros/latex/contrib/outline/outline.sty
+wget -NP sty http://mirrors.ctan.org/macros/latex/contrib/hvfloat/hvfloat.sty
 
 # cls
 wget -NP cls http://mirrors.ctan.org/macros/latex/contrib/hitec/hitec.cls
 wget -NP cls http://mirrors.ctan.org/macros/latex/contrib/hletter/hletter.cls
 
 # LaTeX
-
 wget -NP latex http://mirrors.ctan.org/macros/latex/contrib/outline/outline.tex
+wget -NP latex http://mirrors.ctan.org/macros/latex/contrib/hvfloat/hvfloat.tex
